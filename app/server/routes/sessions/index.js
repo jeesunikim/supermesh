de2be2d451a52f7964e9edff85190b5203283a1b
Jeesun Kim <jeesunikim@gmail.com>
2015-09-23 19:29:04 -0400

    session route update

---
+++ app/server/routes/sessions/index.js
@@ -0,0 +1,12 @@
+var router = require('express').Router(),
+	Session = mongoose.model('Session');
+
+// router.post('/session', function (req, res, next) {
+// 	Session.create(req.body);
+// });
+
+// router.get('/session', function (req, res, next) {
+// 	Session.create(req.body);
+// });
+
+module.exports = router;
