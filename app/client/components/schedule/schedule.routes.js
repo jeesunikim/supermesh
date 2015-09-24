7f59acfa1a0865e7238a010c32fba8616b46afa8
Mark Hansen <x@markthemark.com>
2015-09-24 13:11:52 -0400

    fixed typo

--- app/client/components/schedule/schedule.routes.js
+++ app/client/components/schedule/schedule.routes.js
@@ -11,7 +11,7 @@
 		function getStates(){
 			return [
 				{
-					state: '/schedule',
+					state: 'schedule',
 					config: {
 						url: '/',
 						templateUrl: 'client/components/schedule/schedule.html'
