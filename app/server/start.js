8a95226da6ffe9c32737256c78e97c47dbe38972
Jeesun Kim <jeesunikim@gmail.com>
2015-10-06 20:48:46 -0400

    mongoose start.js

--- app/server/start.js
+++ app/server/start.js
@@ -1,13 +1,25 @@
 'use strict';
 
 var chalk = require('chalk'),
-    app = require('./routes/configure/');
+    app = require('./routes/configure/'),
+    db = require('./db/db');
 
 var server = require('http').createServer();
 
-server.on('request', app); // Attach the Express Application
+var createApplication = function() {
+    server.on('request', app); // Attach the Express Application
+    // require('./io')(server); //Attach socket.io
+};
 
-var port = 7777;
-server.listen(port, function() {
-    console.log(chalk.blue('Server started on port', chalk.magenta(port)));
+var startServer = function () {
+        var port = 7777;
+        server.listen(port, function() {
+            console.log(chalk.blue('Server started on port', chalk.magenta(port)));
+        });
+};
+
+db.then(createApplication).then(startServer).catch(function(err) {
+    console.error('Initialization error:', chalk.red(err.message));
+    console.error('Process terminating . . .');
+    process.kill(1);
 });
