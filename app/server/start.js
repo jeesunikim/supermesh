bde3c6456f9e8c6e0c726c9b8879ee4e1d9907de
Jeesun Kim <jeesunikim@gmail.com>
2015-11-22 21:55:26 -0500

    restructuring

--- app/server/start.js
+++ app/server/start.js
@@ -1,35 +1,26 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+'use strict';
 
-var chalk = require('chalk'),
-    config = require('./config/config'),
-    mongoose = require('./config/mongoose'),
-    express = require('./config/express'),
-    app = require('./routes/configure/'),
-    db = mongoose(),
-    app = express();
+var chalk = require('chalk');
+var startDb = require('./db');
+var server = require('http').createServer();
 
+var createApplication = function() {
+    var app = require('./routes');
+    server.on('request', app); // Attach the Express application.
+    require('./io')(server); // Attach socket.io.
+};
 
-app.listen(config.port);
+var startServer = function() {
+    process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+    server.listen(PORT, function() {
+        console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
+    });
+};
 
-module.exports = app;
+startDb.then(createApplication).then(startServer).catch(function(err) {
+    console.error('Initialization error:', chalk.red(err.message));
+    console.error('Process terminating . . .');
+    process.kill(1);
+});
+
 console.log(process.env.NODE_ENV  + ' server running at http://localhost:' + config.port);
-
-// var server = require('http').createServer();
-
-// var createApplication = function() {
-//     server.on('request', app); // Attach the Express Application
-//     // require('./io')(server); //Attach socket.io
-// };
-
-// var startServer = function () {
-//         var port = 7777;
-//         server.listen(port, function() {
-//             console.log(chalk.blue('Server started on port', chalk.magenta(port)));
-//         });
-// };
-
-// db.then(createApplication).then(startServer).catch(function(err) {
-//     console.error('Initialization error:', chalk.red(err.message));
-//     console.error('Process terminating . . .');
-//     process.kill(1);
-// });
