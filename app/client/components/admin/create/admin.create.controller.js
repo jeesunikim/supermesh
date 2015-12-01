1b28093aa576c353b24a82f00158d75f41543af2
Jeesun Kim <jeesunikim@gmail.com>
2015-12-01 15:20:34 -0500

    PB fall season images added

--- app/client/components/admin/create/admin.create.controller.js
+++ app/client/components/admin/create/admin.create.controller.js
@@ -4,19 +4,19 @@
 	.module('sm.adminCreate')
 	.controller('AdminCreateController', AdminCreateController);
 
-	AdminCreateController.$inject = ['$scope','$rootScope','AdminFactory', '$firebaseObject','$firebaseArray','FIREBASE_URI'];
+	AdminCreateController.$inject = ['$scope', '$stateParams', '$state', '$rootScope','AdminFactory', '$firebaseObject','$firebaseArray','FIREBASE_URI'];
 
-	function AdminCreateController($scope, $rootScope, AdminFactory, $firebaseObject, $firebaseArray, FIREBASE_URI){
+	function AdminCreateController($scope, $stateParams, $state, $rootScope, AdminFactory, $firebaseObject, $firebaseArray, FIREBASE_URI){
 		var eventRef = new Firebase(FIREBASE_URI + 'Session');
 
 		//instatiate session object and add to $scope
 		$scope.session = {
-			name: "",
-			category: "",
-			desc: "",
-			budget: "",
-			address: "",
-			imgUrl:""
+			// name: "",
+			// category: "",
+			desc: ""
+			// budget: "",
+			// address: "",
+			// imgUrl:""
 		};
 
 
@@ -30,23 +30,23 @@
 		};
 
 		//watch file for change, do stuff and trigger factory Func.
-		$scope.$watch('file', function(file){
-			if(file){
-				$scope.loading = true;
+		// $scope.$watch('file', function(file){
+		// 	if(file){
+		// 		$scope.loading = true;
 
-				AdminFactory.uploadImg($scope.file).then(function(data){
-					$scope.loading = false;
-					console.log(data, "data");
-					$scope.upload = data;
+		// 		AdminFactory.uploadImg($scope.file).then(function(data){
+		// 			$scope.loading = false;
+		// 			console.log(data, "data");
+		// 			$scope.upload = data;
 
-		//get path on callback from AWS
-		var initial_path ="https://s3-us-west-2.amazonaws.com/supermesh/";
-		//this is necessary because of the way AWS formats spaces "+"
-		$scope.session.imgUrl = initial_path + data.data.split(' ').join('+');
+		// //get path on callback from AWS
+		// var initial_path ="https://s3-us-west-2.amazonaws.com/supermesh/";
+		// //this is necessary because of the way AWS formats spaces "+"
+		// $scope.session.imgUrl = initial_path + data.data.split(' ').join('+');
 
-		});
-			}
-		})
+		// });
+		// 	}
+		// })
 
 	}
 })();
